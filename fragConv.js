0419617b4f9e25c2e5ec78aa320cf3843759523c
Keshav Murali <keshav88.m@gmail.com>
2021-03-31 21:16:46 +0530

    Added icons+functions to insert new items and rudimentary CSS to highlight fragments and items

--- fragConv.js
+++ fragConv.js
@@ -41,10 +41,10 @@ function fragJSONToDOM(str)
     DOMObject.id = obj.id;
     DOMObject.className = "fragment"
 
-    // needs to generate icons, not done yet
-    DOMObject.innerHTML = "<h2 class='title'>" + obj.title + "</h2>" +
+    var s = "<h2 class='title'>" + obj.title + "</h2>" +
 	"<span class='fragid'>id: " + obj.id + "<br></span>" + "<div class='tags'><h3>Tags</h3>" + "<span class='tags_list'><ul style='list-style-type: none'></ul></span></div>";
-
+    s += "<div class='icons'><img src='/icon/inserttext.png' onclick='insertNewText(this.parentNode.parentNode)'> <img src='/icon/insertimage.png' onclick='insertNewImage(this.parentNode.parentNode)'> <img src='/icon/insertaudio.png' onclick='insertNewAudio(this.parentNode.parentNode)'> <img src='/icon/insertvideo.png' onclick='insertNewVideo(this.parentNode.parentNode)'> <img src='/icon/link.png' onclick='insertNewLink(this.parentNode.parentNode)'> </div>"
+    DOMObject.innerHTML = s;
     target = DOMObject.getElementsByTagName("ul")[0];
     
     for(var i = 0; i < obj.tags.length; i++)
@@ -58,4 +58,44 @@ function fragJSONToDOM(str)
 	target.appendChild(JSONtoDOM(JSON.stringify(obj.items[i])));
 				
     return DOMObject;
+}
+
+function insertNewText(obj)
+{
+    var cont = obj.getElementsByClassName("content")[0];
+    var def_text = {"type":"text","content":"\n\t <p>Lorem ipsum</p>"}
+    
+    cont.appendChild(JSONtoDOM(JSON.stringify(def_text)));
+}
+
+function insertNewImage(obj)
+{
+    var cont = obj.getElementsByClassName("content")[0];
+    var def_image = {"type":"image","desc":"Default image","content":"/includes/defimg.png"}
+    
+    cont.appendChild(JSONtoDOM(JSON.stringify(def_image)));
+}
+
+function insertNewAudio(obj)
+{
+    var cont = obj.getElementsByClassName("content")[0];
+    var def_audio = {"type":"audio","desc":"Default audio","content":"/includes/defaud.ogg"}
+    
+    cont.appendChild(JSONtoDOM(JSON.stringify(def_audio)));
+}
+
+function insertNewVideo(obj)
+{
+    var cont = obj.getElementsByClassName("content")[0];
+    var def_video = {"type":"video","desc":"Default video","content":"/includes/defvid.webm"}
+    
+    cont.appendChild(JSONtoDOM(JSON.stringify(def_video)));
+}
+
+function insertNewLink(obj)
+{
+    var cont = obj.getElementsByClassName("content")[0];
+    var def_link = {"type":"link","original_link":"https://www.yahoo.com/","original_text":"Default link","archived_link":""}
+    
+    cont.appendChild(JSONtoDOM(JSON.stringify(def_link)));
 }
